022c5356a924de9da13df3404be50ca8bf92dcbc
Sewanu Projects <55129150+sewanu@users.noreply.github.com>
2022-12-07 16:56:10 -0500

    Update s3_backend.tf

--- 03_02-03_remotestate/remote_resources/s3_backend.tf
+++ 03_02-03_remotestate/remote_resources/s3_backend.tf
@@ -6,7 +6,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 variable "bucket_name" {
-  default = "red30-tfstate"
+  default = "sred30-tfstate"
 }
 
 # //////////////////////////////
@@ -69,7 +69,7 @@ resource "aws_s3_bucket_public_access_block" "red30-tfremotestate" {
 # DYNAMODB TABLE
 # //////////////////////////////
 resource "aws_dynamodb_table" "tf_db_statelock" {
-  name           = "red30-tfstatelock"
+  name           = "sred30-tfstatelock"
   read_capacity  = 20
   write_capacity = 20
   hash_key       = "LockID"
